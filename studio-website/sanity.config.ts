da2a2867eef82d8b98d3d3640b1d84aeabcb039b
Marvinkwame <marvinkudjo@gmail.com>
2025-01-29 16:13:34 +0000

    Fixed

--- studio-website/sanity.config.ts
+++ studio-website/sanity.config.ts
@@ -1,18 +1,26 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 
-export default defineConfig({
-  name: 'default',
-  title: 'Website',
+const plugins = [structureTool()]
 
-  projectId: 'ixkm24zi',
+if (process.env.NODE_ENV === 'development') {
+  const {visionTool} = await import('@sanity/vision')
+  plugins.push(visionTool())
+}
+
+const config = defineConfig({
+  name: 'default',
+  title: 'Your Project Name',
+
+  projectId: 'your-project-id',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins,
 
   schema: {
     types: schemaTypes,
   },
 })
+
+export default config
